e1c452e3d364cac28d2163bb9f75af3dde6a5cad
Grant Surlyn <grant@sunflowerseastar.com>
2022-10-21 10:37:52 -0400

    prevent multi-digit text input value (from key smash)
    also, add comments

--- src/sudoku_clojure/core.cljs
+++ src/sudoku_clojure/core.cljs
@@ -1,6 +1,6 @@
 (ns ^:figwheel-hooks sudoku-clojure.core
   (:require
-   [clojure.string :refer [replace-first]]
+   [clojure.string :refer [replace]]
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom create-class]]
    [reagent.dom :as rdom]
@@ -61,7 +61,21 @@
     (reset! is-board-pristine false)
     (swap! board assoc-in [y x] new-value)))
 
-(defn square-c [x y square update-board-fn]
+(defn square-c
+  "This is the text-input component where the square's number is shown and can be
+  edited. It is painfully complicated for a few reasons:
+    - The sudoku logic needs the input values to be integers, while regexp'ing and
+        on-change handing needs to use strings (and browser text inputs will end up
+        changing ints to strings when given the chance). So it requires care to handle
+        it as a string, but keep the value at rest as an int.
+    - It's special/custom input handling, in that a number press (other than 0)
+        immediately updates the field as the new, single number; a
+        backspace/delete/space clears the field; an alpha, special, or 0 press does nothing.
+    - If the user presses many numbers all at the same time, the browser will dump
+        all the numbers into the on-change function's `(->> % .-target .-value)`
+        together, meaning that the 'just keep the value as a single digit' logic needs
+        to handle yet another edge case."
+  [x y square update-board-fn]
   [:div.square
    {:style {:grid-column (+ x 1) :grid-row (+ y 1)}}
    [:input
@@ -70,17 +84,30 @@
      #(let [;; "value" means "the value of the text input field"
             new-value-as-entered (->> % .-target .-value)
             old-value (str square)
+
             ;; if ex. there was '2' and the user pressed '5', then the new-value-as-entered would be '25'.
-            ;; In this case, remove the old value, '2', from '25'. new-value will be '5'.
-            new-value (if (> (count new-value-as-entered) (count old-value))
-                        (replace-first new-value-as-entered (re-pattern old-value) "")
-                        new-value-as-entered)
-            ;; if it's a 1 through 9, use it (as an int, not a string)
+            ;; In this case, remove the old value, '2', from '25'. new-value-without-old-value will be '5'.
+            new-value-without-old-value
+            (if (> (count new-value-as-entered) (count old-value))
+              (replace new-value-as-entered (re-pattern old-value) "")
+              new-value-as-entered)
+
+            ;; If a chaos-monkey-esque user presses many numbers at the same time, the
+            ;; new-value-as-entered can be many digits. This is because the browser will
+            ;; batch the input's on-change, as opposed to running this on-change
+            ;; function each time (which would keep the input always at one
+            ;; digit at most). For this edge case, just keep the last digit only.
+            new-value-stripped
+            (if (> (count new-value-without-old-value) 1)
+              (subs new-value-without-old-value (dec (count new-value-without-old-value)) (count new-value-without-old-value))
+              new-value-without-old-value)
+
+            ;; if it's a 1 through 9, use it (and use it as an int, not a string)
             new-value-validated
-            (cond (re-find #"[1-9]" new-value) (js/parseInt new-value)
+            (cond (re-find #"[1-9]" new-value-stripped) (js/parseInt new-value-stripped)
                   ;; if it's blank (user pressed delete/backspace), empty it --
                   ;; remember that 0 (as an int) is used in the logic as "all choices"
-                  (or (= new-value "") (= new-value " ")) 0
+                  (or (= new-value-stripped "") (= new-value-stripped " ")) 0
                   ;; otherwise (user pressed alpha or other key), just leave it --
                   ;; note that this uses 'square', not 'old-value', which is a string
                   :else (js/parseInt square))]
